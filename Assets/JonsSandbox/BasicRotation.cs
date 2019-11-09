bc40a5d69c8185efe4c4ab1fe661b3c9090ef818
Jon Collins <jon@ki-games.com>
2019-11-09 14:33:11 -0800

    Preview Completed

---
+++ Assets/JonsSandbox/BasicRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicRotation : MonoBehaviour
+{
+    public float speed = .3f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, speed, 0));
+    }
+}
