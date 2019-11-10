65cf0317d9925eb2ad7b23601e73609aec6839a1
Fabian Fabro <fabrofabian@gmail.com>
2019-11-09 21:08:11 -0800

    cha-ching SFX to buying a piece

--- Assets/_OSHACoaster/Scripts/SFXPlay.cs
+++ Assets/_OSHACoaster/Scripts/SFXPlay.cs
@@ -5,59 +5,22 @@ using UnityEngine.UI;
 
 public class SFXPlay : MonoBehaviour
 {
-    FMOD.Studio.EventInstance PlayMoneySound;
-    FMOD.Studio.PLAYBACK_STATE playbackState;
-    //public Button buyButton;
 
-    //void Awake()
-    //{
-    //    PlayMoneySound = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/BoughtItem");
 
-    //}
 
     void start()
     {
-        PlayMoneySound = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/BoughtItem");
-        //Button btn = buyButton.GetComponent<Button>();
-        //btn.onClick.AddListener(taskToClick);
-        //btn.onClick.AddListener(playSound);
-        //PlayMoneySound.start();
+
 
     }
 
     public void playSound()
     {
-        PlayMoneySound.getPlaybackState(out playbackState);
-        //PlayMoneySound.start();
-        Debug.Log(PlayMoneySound);
-        Debug.Log(playbackState);
-        //Debug.Log(PlayMoneySound);
-        //if (playbackState != FMOD.Studio.PLAYBACK_STATE.STOPPED)
-        //{
-        //    PlayMoneySound.start();
 
-        //}
-        //Debug.Log(playbackState == FMOD.Studio.PLAYBACK_STATE.STOPPED);
         
     }
 
 
-    //void playSound()
-    //{
-    //    PlayMoneySound.getPlaybackState(out playbackState);
-    //    Debug.Log(playbackState == FMOD.Studio.PLAYBACK_STATE.STOPPED);
-    //    PlayMoneySound.start();
 
-
-    //    //layMoneySound.start();
-    //    //Debug.Log("Clicked");
-    //}
-
-    // Update is called once per frame
-    //void Update()
-    //{
-    //    Button btn = buyButton.GetComponent<Button>();
-    //    btn.onClick.AddListener(playSound);
-    //}
 }
 
