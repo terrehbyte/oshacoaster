bee39830587070bee23fdfb1f79da7453c6f627a
Jon Collins <jon@ki-games.com>
2019-11-10 11:51:57 -0800

    Working on endgame screen

--- Assets/_OSHACoaster/Scripts/GamePlay.cs
+++ Assets/_OSHACoaster/Scripts/GamePlay.cs
@@ -9,6 +9,7 @@ public class GamePlay : Object
     public static int coin = 1000;
     public static int totalKillsAllTime;
     public static int killsThisSession;
+    public static int casualtiesThisSession;
     public static  Dictionary<string,BaseItem> inventory;
     public static GridController grid;
     //TOOD: creat the datastructures to hold the tilemap data & placement of built items.
