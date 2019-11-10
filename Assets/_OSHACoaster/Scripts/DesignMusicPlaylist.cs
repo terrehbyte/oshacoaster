0be067a58f5fd56b75b500f59b9fe27e3920cf23
Fabian Fabro <fabrofabian@gmail.com>
2019-11-09 22:58:15 -0800

    trying to make the design music playlist and added an audio mixer object

---
+++ Assets/_OSHACoaster/Scripts/DesignMusicPlaylist.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesignMusicPlaylist : MonoBehaviour
+{
+    public AudioClip[] designModeMusic;
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = FindObjectOfType<AudioSource>();
+        audioSource.loop = false;
+    }
+
+    private AudioClip GetRandomClip()
+    {
+        return designModeMusic[Random.Range(0, designModeMusic.Length)];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!audioSource.isPlaying)
+        {
+            audioSource.clip = GetRandomClip();
+            audioSource.Play();
+        }
+    }
+}
