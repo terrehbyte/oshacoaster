359c026ea29d5075791dc5be075536249cc2a969
Terry Nguyen <terreh@terrehbyte.com>
2019-11-09 18:42:36 -0800

    Connect nearby rail blocks

---
+++ Assets/_OSHACoaster/Scripts/RailConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RailConfiguration : MonoBehaviour
+{
+    public RailConfiguration nextRailTilePosition;
+
+    public Vector3Int GetTilePosition()
+    {
+        return GamePlay.grid.GetTileLocation(transform.position);
+    }
+
+    void OnDrawGizmos()
+    {
+        if (nextRailTilePosition == null) { return; }
+        Gizmos.color = Color.white;
+        Gizmos.DrawLine(transform.position, nextRailTilePosition.transform.position);
+    }
+}
