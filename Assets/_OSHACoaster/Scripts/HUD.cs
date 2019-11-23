a72cb951989caa335395cde8030b2a3553e33cc5
Chris <chris.ll.dougherty@gmail.com>
2019-11-23 15:51:13 -0800

    hud

---
+++ Assets/_OSHACoaster/Scripts/HUD.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMP = TMPro.TextMeshProUGUI;
+
+public class HUD : MonoBehaviour
+{
+    public TMP monies;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        monies.text =  "Balance:" + GamePlay.coin.ToString();
+    }
+}
